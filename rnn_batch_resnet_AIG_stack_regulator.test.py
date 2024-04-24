1c4ba9e4897a48aed14dc8f1d9753729f668d8ca
lyn <397392686@qq.com>
2024-04-24 23:33:24 +0800

    Initial commit

---
+++ rnn_batch_resnet_AIG_stack_regulator.test.py
@@ -0,0 +1,146 @@
+import keras
+import tensorflow as tf
+from keras.callbacks import EarlyStopping
+from keras.layers import Activation
+from keras.initializers import GlorotUniform
+
+from utils import generate_add, compute_accuracy, generate_same, generate_convert, generate_binary_and, \
+    generate_binary_or, generate_binary_not, generate_binary_xor
+
+input_num = 8
+output_num = 4
+lut_num = 8
+lut_num1 = 8
+lut_num2 = 8
+lut_num3 = 8
+lut_num4 = 8
+lut_num5 = 8
+lut_num6 = 8
+reg_num = 8
+
+ic_input_num = 2 * input_num + 1
+ic1_input_num = 4 * lut_num + 1
+ic2_input_num = 4 * lut_num1 + 1
+ic3_input_num = 4 * lut_num2 + 1
+ic4_input_num = 4 * lut_num3 + 1
+ic5_input_num = 4 * lut_num4 + 1
+ic6_input_num = 4 * lut_num5 + 1
+ic7_input_num = 4 * lut_num6 + 1
+
+
+def lut_operation(num_luts, inputs):
+    # | id | Operator             | AB=00 | AB=01 | AB=10 | AB=11 |
+    # |----|----------------------|-------|-------|-------|-------|
+    # | 0  | 0                    | 0     | 0     | 0     | 0     |
+    # | 1  | A and B              | 0     | 0     | 0     | 1     |
+    # | 2  | not(A implies B)     | 0     | 0     | 1     | 0     |
+    # | 3  | A                    | 0     | 0     | 1     | 1     |
+    # | 4  | not(B implies A)     | 0     | 1     | 0     | 0     |
+    # | 5  | B                    | 0     | 1     | 0     | 1     |
+    # | 6  | A xor B              | 0     | 1     | 1     | 0     |
+    # | 7  | A or B               | 0     | 1     | 1     | 1     |
+    # | 8  | not(A or B)          | 1     | 0     | 0     | 0     |
+    # | 9  | not(A xor B)         | 1     | 0     | 0     | 1     |
+    # | 10 | not(B)               | 1     | 0     | 1     | 0     |
+    # | 11 | B implies A          | 1     | 0     | 1     | 1     |
+    # | 12 | not(A)               | 1     | 1     | 0     | 0     |
+    # | 13 | A implies B          | 1     | 1     | 0     | 1     |
+    # | 14 | not(A and B)         | 1     | 1     | 1     | 0     |
+    # | 15 | 1                    | 1     | 1     | 1     | 1     |
+    outputs = []
+    for i in range(num_luts):
+        # Extract weights for the i-th LUT
+        a = inputs[:, 2 * i]
+        b = inputs[:, 2 * i + 1]
+        # output = 2.0* tf.maximum(0.0, a*b - 0.5)
+        # output = 1 / (1 + tf.exp(-50 * (a*b - 0.5)))
+        output = a * b
+        # Apply the LUT logic
+        outputs.append(output)
+    return tf.stack(outputs, axis=-1)
+
+
+class FeedForward(keras.Model):
+    def __init__(self, units, batch_size=200):
+        self.units = units
+        self.batch_size = batch_size
+        super(FeedForward, self).__init__()
+        self.wic = self.add_weight(shape=(4 * lut_num, ic_input_num),
+                                   initializer=GlorotUniform(),
+                                   trainable=True,
+                                   )
+        self.wic1 = self.add_weight(shape=(4 * lut_num1, ic1_input_num),
+                                    initializer=GlorotUniform(),
+                                    trainable=True,
+                                    )
+        self.wic2 = self.add_weight(shape=(4 * lut_num2, ic2_input_num),
+                                    initializer=GlorotUniform(),
+                                    trainable=True,
+                                    )
+        self.wic3 = self.add_weight(shape=(4 * lut_num3, ic3_input_num),
+                                    initializer=GlorotUniform(),
+                                    trainable=True,
+                                    )
+        self.wic4 = self.add_weight(shape=(output_num, ic4_input_num),
+                                    initializer=GlorotUniform(),
+                                    trainable=True,
+                                    )
+
+    def call(self, inputs):
+        softmax_weights = tf.nn.softmax(self.wic)
+        ones = tf.expand_dims(tf.ones([tf.shape(inputs)[0]], dtype=inputs.dtype), -1)
+        combined_inputs = tf.concat([inputs, 1 - inputs, ones], axis=-1)
+        ic_output = tf.matmul(combined_inputs, tf.transpose(softmax_weights))
+        ic_outputa, ic_outputb = tf.split(ic_output, 2, axis=-1)
+        lut_output = lut_operation(lut_num, ic_outputa)
+
+        softmax_weights1 = tf.nn.softmax(self.wic1)
+        combined_inputs1 = tf.concat([ic_outputb, lut_output, 1 - lut_output, ones], axis=-1)
+        ic_output1 = tf.matmul(combined_inputs1, tf.transpose(softmax_weights1))
+        ic_outputa, ic_outputb = tf.split(ic_output1, 2, axis=-1)
+        lut_output1 = lut_operation(lut_num1, ic_outputa)
+
+        # softmax_weights2 = tf.nn.softmax(self.wic2)
+        # combined_inputs2 = tf.concat([ic_outputb, lut_output1, 1 - lut_output1, ones], axis=-1)
+        # ic_output2 = tf.matmul(combined_inputs2, tf.transpose(softmax_weights2))
+        # ic_outputa, ic_outputb = tf.split(ic_output2, 2, axis=-1)
+        # lut_output2 = lut_operation(lut_num2, ic_outputa)
+
+        # softmax_weights3 = tf.nn.softmax(self.wic3)
+        # combined_inputs3 = tf.concat([ic_outputb, lut_output, 1 - lut_output, ones], axis=-1)
+        # ic_output3 = tf.matmul(combined_inputs3, tf.transpose(softmax_weights3))
+        # ic_outputa, ic_outputb = tf.split(ic_output3, 2, axis=-1)
+        # lut_output3 = lut_operation(lut_num3, ic_outputa)
+
+        softmax_weights4 = tf.nn.softmax(self.wic4, axis=-1)
+        ones = tf.expand_dims(tf.ones([tf.shape(lut_output1)[0]], dtype=inputs.dtype), -1)
+        combined_inputs4 = tf.concat([ic_outputb, lut_output1, 1 - lut_output1, ones], axis=-1)
+        ic_output4 = tf.matmul(combined_inputs4, tf.transpose(softmax_weights4))
+
+        return ic_output4
+
+
+num_epochs = 200
+batch_size = 32
+# 创建FeedForward层的实例，神经元个数为units
+model = FeedForward(units=256)
+
+# Generate data using the modified function
+x_train, y_train, x_val, y_val, x_test, y_test = generate_binary_xor(4, batch_size * 3000)
+# inputs,outputs=generate_binary_and(8,batch_size*1000)
+# inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
+# outputs = tf.convert_to_tensor(outputs, dtype=tf.float32)
+
+x_train = tf.convert_to_tensor(x_train, dtype=tf.float32)
+y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
+x_test = tf.convert_to_tensor(x_test, dtype=tf.float32)
+
+# 编译模型
+model.compile(optimizer='adam', loss='mse')
+early_stopping = EarlyStopping(monitor='loss', patience=10, verbose=1, min_delta=0.0001)
+model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, callbacks=[early_stopping])
+
+# 进行预测
+predictions = model.predict(x_test)
+accuracy = compute_accuracy(predictions, y_test)
+print("accuracy=", accuracy)
---
+++ utils.py
@@ -0,0 +1,260 @@
+import numpy as np
+import tensorflow as tf
+input_num = 16
+output_num = 8
+lut_num = 20
+lut_num1 = 20
+lut_num2 = 20
+lut_num3 = 20
+lut_num4 = 20
+lut_num5 = 20
+lut_num6 = 20
+reg_num = 8
+
+ic_input_num = 2 * (input_num + reg_num) + 1
+ic1_input_num = 4 * lut_num + 1
+ic2_input_num = 4 * lut_num1 + 1
+ic3_input_num = 4 * lut_num2 + 1
+ic4_input_num = 4 * lut_num3 + 1
+ic5_input_num = 4 * lut_num4 + 1
+ic6_input_num = 4 * lut_num5 + 1
+ic7_input_num = 4 * lut_num6 + 1
+
+
+# 创建相反的数据
+def generate_convert(binary_dimension,total_samples):
+    def int_to_binary(n):
+        string_num=bin(n)[2:]
+        # 使用np.array将列表转换为NumPy数组
+        int_list_left = [int(char) for char in string_num]
+        num_len=len(int_list_left)
+        binary = [0]*(binary_dimension-num_len)+int_list_left
+        return binary
+    inputs = []
+    outputs = []
+    for _ in range (total_samples):
+        num = np.random.randint(0, 64)
+    # 将数字转换为二进制数组
+        binary_left = int_to_binary(num)
+        inputs.append(binary_left[:])
+        for i in range (len(binary_left)):
+            binary_left[i]=1-binary_left[i]
+        outputs.append(binary_left)
+    return inputs,outputs
+
+
+
+# 创建相等的数据
+def generate_same(binary_dimension,total_samples):
+    def int_to_binary(n):
+        string_num=bin(n)[2:]
+        # 使用np.array将列表转换为NumPy数组
+        int_list_left = [int(char) for char in string_num]
+        num_len=len(int_list_left)
+        binary = [0]*(binary_dimension-num_len)+int_list_left
+        return binary
+    inputs = []
+    outputs = []
+    for _ in range (total_samples):
+        num = np.random.randint(0, 64)
+    # 将数字转换为二进制数组
+        binary_left = int_to_binary(num)
+        inputs.append(binary_left)
+        outputs.append(binary_left)
+    return inputs,outputs
+
+# 创建加法数据
+def generate_add(binary_dimension,total_samples):
+    def int_to_binary(n):
+        string_num=bin(n)[2:]
+        # 使用np.array将列表转换为NumPy数组
+        int_list_left = [int(char) for char in string_num]
+        num_len=len(int_list_left)
+        binary = [0]*(binary_dimension-num_len)+int_list_left
+        return binary
+    inputs=[]
+    outputs=[]
+    for _ in range (total_samples):
+        left=np.random.randint(0,64)
+        right=np.random.randint(0,64)
+        # 将数字转换为二进制数组
+        binary_left = int_to_binary(left)
+        binary_right = int_to_binary(right)
+        # 计算
+        sum_value = left + right
+        sum_value=int_to_binary(sum_value)
+        # 将二进制输入和真实和作为一对数据添加到输入列表中
+        inputs.append(binary_left+binary_right)
+        outputs.append(sum_value)
+    # split dataset
+    split_train = int(0.7 * len(inputs))
+    split_val = int(0.8 * len(inputs))
+    x_train = inputs[:split_train]
+    y_train = outputs[:split_train]
+    x_val = inputs[split_train:split_val]
+    y_val = outputs[split_train:split_val]
+    x_test = inputs[split_val:]
+    y_test = outputs[split_val:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+
+# 创建二进制取and数据
+def generate_binary_and(binary_dimension,total_samples):
+    def int_to_binary(n):
+        string_num=bin(n)[2:]
+        # 使用np.array将列表转换为NumPy数组
+        int_list_left = [int(char) for char in string_num]
+        num_len=len(int_list_left)
+        binary = [0]*(binary_dimension-num_len)+int_list_left
+        return binary
+    inputs=[]
+    outputs=[]
+    for _ in range (total_samples):
+        left=np.random.randint(0,64)
+        right=np.random.randint(0,64)
+        # 将数字转换为二进制数组
+        binary_left = int_to_binary(left)
+        binary_right = int_to_binary(right)
+        res=[]
+        for i in range (len(binary_left)):
+            res.append(binary_left[i]*binary_right[i])
+        # 将二进制输入和真实和作为一对数据添加到输入列表中
+        inputs.append(binary_left+binary_right)
+        outputs.append(res)
+    # split dataset
+    split_train = int(0.7 * len(inputs))
+    split_val = int(0.8 * len(inputs))
+    x_train = inputs[:split_train]
+    y_train = outputs[:split_train]
+    x_val = inputs[split_train:split_val]
+    y_val = outputs[split_train:split_val]
+    x_test = inputs[split_val:]
+    y_test = outputs[split_val:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+# 创建二进制取or数据
+def generate_binary_or(binary_dimension,total_samples):
+    def int_to_binary(n):
+        string_num=bin(n)[2:]
+        # 使用np.array将列表转换为NumPy数组
+        int_list_left = [int(char) for char in string_num]
+        num_len=len(int_list_left)
+        binary = [0]*(binary_dimension-num_len)+int_list_left
+        return binary
+    inputs=[]
+    outputs=[]
+    for _ in range (total_samples):
+        left=np.random.randint(0,256)
+        right=np.random.randint(0,256)
+        # 将数字转换为二进制数组
+        binary_left = int_to_binary(left)
+        binary_right = int_to_binary(right)
+        res=[]
+        for i in range (len(binary_left)):
+            res.append(binary_left[i]+binary_right[i]-binary_left[i]*binary_right[i])
+        # 将二进制输入和真实和作为一对数据添加到输入列表中
+        inputs.append(binary_left+binary_right)
+        outputs.append(res)
+    # split dataset
+    split_train = int(0.7 * len(inputs))
+    split_val = int(0.8 * len(inputs))
+    x_train = inputs[:split_train]
+    y_train = outputs[:split_train]
+    x_val = inputs[split_train:split_val]
+    y_val = outputs[split_train:split_val]
+    x_test = inputs[split_val:]
+    y_test = outputs[split_val:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
+# 创建二进制取xor数据
+def generate_binary_xor(binary_dimension,total_samples):
+    def int_to_binary(n):
+        string_num=bin(n)[2:]
+        # 使用np.array将列表转换为NumPy数组
+        int_list_left = [int(char) for char in string_num]
+        num_len=len(int_list_left)
+        binary = [0]*(binary_dimension-num_len)+int_list_left
+        return binary
+    inputs=[]
+    outputs=[]
+    for _ in range (total_samples):
+        left=np.random.randint(0,16)
+        right=np.random.randint(0,16)
+        # 将数字转换为二进制数组
+        binary_left = int_to_binary(left)
+        binary_right = int_to_binary(right)
+        res=[]
+        for i in range (len(binary_left)):
+            res.append(binary_left[i]^binary_right[i])
+        # 将二进制输入和真实和作为一对数据添加到输入列表中
+        inputs.append(binary_left+binary_right)
+        outputs.append(res)
+    # split dataset
+    split_train = int(0.7 * len(inputs))
+    split_val = int(0.8 * len(inputs))
+    x_train = inputs[:split_train]
+    y_train = outputs[:split_train]
+    x_val = inputs[split_train:split_val]
+    y_val = outputs[split_train:split_val]
+    x_test = inputs[split_val:]
+    y_test = outputs[split_val:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
+# 创建二进制取not数据
+def generate_binary_not(binary_dimension,total_samples):
+    def int_to_binary(n):
+        string_num=bin(n)[2:]
+        # 使用np.array将列表转换为NumPy数组
+        int_list_left = [int(char) for char in string_num]
+        num_len=len(int_list_left)
+        binary = [0]*(binary_dimension-num_len)+int_list_left
+        return binary
+    inputs=[]
+    outputs=[]
+    for _ in range (total_samples):
+        left=np.random.randint(0,64)
+        right=np.random.randint(0,64)
+        # 将数字转换为二进制数组
+        binary_left = int_to_binary(left)
+        binary_right = int_to_binary(right)
+        res=[]
+        for i in range (len(binary_left)):
+            res.append(1-binary_left[i])
+        # 将二进制输入和真实和作为一对数据添加到输入列表中
+        inputs.append(binary_left[:]+binary_right)
+        outputs.append(res)
+    # split dataset
+    split_train = int(0.7 * len(inputs))
+    split_val = int(0.8 * len(inputs))
+    x_train = inputs[:split_train]
+    y_train = outputs[:split_train]
+    x_val = inputs[split_train:split_val]
+    y_val = outputs[split_train:split_val]
+    x_test = inputs[split_val:]
+    y_test = outputs[split_val:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
+def lut_operation(num_luts, inputs):
+
+    outputs = []
+    for i in range(num_luts):
+        a = inputs[:, 2 * i]
+        b = inputs[:, 2 * i + 1]
+        output = a * b
+        outputs.append(output)
+    return tf.stack(outputs, axis=-1)
+def compute_accuracy(output, y_test):
+    # 将输出和测试标签展平为一维张量
+    output = tf.reshape(output, [-1])
+    y_test = tf.reshape(y_test, [-1])
+
+    # 将输出和测试标签转换为numpy数组
+    output = output.numpy()
+    y_test = y_test.numpy()
+
+    # 四舍五入输出
+    output = (output > 0.7).astype(int)
+
+    # 计算准确率
+    correct = (output == y_test).sum().item()
+    # 计算准确率
+    accuracy = correct / len(y_test)
+
+    return accuracy
